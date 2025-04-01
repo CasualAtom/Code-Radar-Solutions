6a9df969628cfe9754ef2070aaacc4b06f925b50
Arnav Saini <sainiarnav100@gmail.com>
2025-04-01 09:19:38 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int fibonacci(int n){
+    if (n<=1){
+        return n;
+    }
+    else{
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+}
+int main(){
+    int n,i,s;
+    scanf("%d ",&n);
+    for(i=s;i<=n;i++){
+        int res = fibonacci(i);
+        printf("%d ", res);
+    }
+    return 0;
+}
